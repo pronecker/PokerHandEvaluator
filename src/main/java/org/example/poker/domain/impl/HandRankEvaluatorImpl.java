c655066f28ec9172927e787f07f2f9f4af500355
Patrick Ronecker <pr@freigmbh.de>
2023-03-24 21:42:16 +0100

    implementation HandRankEvaluator

--- src/main/java/org/example/poker/domain/impl/HandRankEvaluatorImpl.java
+++ src/main/java/org/example/poker/domain/impl/HandRankEvaluatorImpl.java
@@ -1,11 +1,9 @@
 package org.example.poker.domain.impl;
 
-import org.example.poker.domain.Card;
-import org.example.poker.domain.HandRank;
-import org.example.poker.domain.HandRankEvaluator;
+import org.example.poker.domain.*;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
+import java.util.*;
 
 public final class HandRankEvaluatorImpl implements HandRankEvaluator {
 
@@ -19,8 +17,88 @@ public final class HandRankEvaluatorImpl implements HandRankEvaluator {
     }
 
     @Override
-    public @NotNull HandRank evaluate(@NotNull Collection<? extends Card> cards) {
+    public @NotNull HandRank evaluate(@NotNull final Collection<? extends Card> cards) {
+        final List<? extends Card> orderedCards = cards.stream()
+                                                       .sorted(Comparator.comparing(Card::getValue).reversed())
+                                                       .toList();
 
-        throw new IllegalStateException("not implemented!");
+        final boolean isFlush = isFlush(cards);
+        final boolean isStraight = isStraight(orderedCards);
+        final Map<CardValue, Integer> valueCounts = getValueCounts(cards);
+
+        if (isFlush && isStraight) {
+            return HandRank.STRAIGHT_FLUSH;
+        }
+
+        if (hasCount(valueCounts, 4)) {
+            return HandRank.FOUR_OF_A_KIND;
+        }
+
+        if (hasCount(valueCounts, 3) && hasCount(valueCounts, 2)) {
+            return HandRank.FULL_HOUSE;
+        }
+
+        if (isFlush) {
+            return HandRank.FLUSH;
+        }
+
+        if (isStraight) {
+            return HandRank.STRAIGHT;
+        }
+
+        if (hasCount(valueCounts, 3)) {
+            return HandRank.THREE_OF_A_KIND;
+        }
+
+        if (countPairs(valueCounts) == 2) {
+            return HandRank.TWO_PAIRS;
+        }
+
+        if (countPairs(valueCounts) == 1) {
+            return HandRank.PAIR;
+        }
+
+        return HandRank.HIGH_CARD;
     }
+
+    private Map<CardValue, Integer> getValueCounts(final Collection<? extends Card> cards) {
+        final Map<CardValue, Integer> valueCounts = new HashMap<>();
+        for (final Card card : cards) {
+            valueCounts.put(card.getValue(),
+                            valueCounts.getOrDefault(card.getValue(), 0) + 1);
+        }
+        return valueCounts;
+    }
+
+    private boolean hasCount(final Map<CardValue, Integer> valueCounts, final int count) {
+        return valueCounts.values().stream().anyMatch(c -> c == count);
+    }
+
+    private int countPairs(final Map<CardValue, Integer> valueCounts) {
+        int pairs = 0;
+        for (final int count : valueCounts.values()) {
+            if (count == 2) {
+                pairs++;
+            }
+        }
+        return pairs;
+    }
+
+    private boolean isFlush(final Collection<? extends Card> cards) {
+        final CardSuit suit = cards.iterator().next().getSuit();
+        return cards.stream().allMatch(c -> c.getSuit() == suit);
+    }
+
+    private boolean isStraight(final List<? extends Card> orderedCards) {
+        CardValue previousValue = orderedCards.iterator().next().getValue();
+        for (int i = 1; i < orderedCards.size(); i++) {
+            final CardValue currentValue = orderedCards.get(i).getValue();
+            if (previousValue.ordinal() - currentValue.ordinal() != 1) {
+                return false;
+            }
+            previousValue = currentValue;
+        }
+        return true;
+    }
+
 }
